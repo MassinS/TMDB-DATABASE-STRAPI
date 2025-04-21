c857e91b6a8cb511b08bd7b307aa3a08f9f53ece
massinS <massinsadi2020@gmail.com>
2025-04-22 00:30:13 +0200

    Modification de schéma de la BDD 1

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -430,6 +430,7 @@ export interface ApiFilmFilm extends Struct.CollectionTypeSchema {
     original_language: Schema.Attribute.String & Schema.Attribute.Required;
     original_title: Schema.Attribute.String & Schema.Attribute.Required;
     overview: Schema.Attribute.Text & Schema.Attribute.Required;
+    page_fetched_from: Schema.Attribute.BigInteger;
     popularity_tmdb: Schema.Attribute.Decimal & Schema.Attribute.Required;
     popularity_website: Schema.Attribute.Decimal;
     poster_path: Schema.Attribute.String & Schema.Attribute.Required;
